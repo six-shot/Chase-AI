3598f235f5917f402b328e38feb5712465b0fde1
six-shot <okhuomonajayi54@gmail.com>
2025-05-13 15:27:18 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -376,12 +376,12 @@ export default function Home() {
               <div className="w-[150px] h-[43px] rounded-[22px] button-gradient relative">
                 <div className="w-full h-full rounded-[22px] bg-black absolute top-[1px] left-[2px] flex items-center">
                   <h6 className="gradient-text ml-4 font-[family-name:var(--font-inter)] font-semibold">
-                    FAQ'S SECTION
+                    FAQ&apos;S SECTION
                   </h6>
                 </div>
               </div>
               <h5 className="text-[44px] font-[family-name:var(--font-inter)] text-[#ffffff] font-medium">
-                Some Common FAQ's
+                Some Common FAQ&apos;s
               </h5>
               <p className="font-[family-name:var(--font-inter)] text-[#E6ECFFB2] text-center font-bold">
                 Get answers to your questions and learn about our platform
