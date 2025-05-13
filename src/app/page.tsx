ff596713d2ee50d83f73ea3b5ad5cdf0f37f0469
six-shot <okhuomonajayi54@gmail.com>
2025-05-13 10:18:04 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,11 @@ import Marquee from "react-fast-marquee";
 import minicard from "../../public/svgs/minicard.svg";
 import star from "../../public/svgs/star.png";
 import check from "../../public/svgs/check.svg";
+import earn from "../../public/svgs/earn.png";
+import grid from "../../public/svgs/grid.png";
+import oneclick from "../../public/svgs/oneclick.png";
+import derivative from "../../public/svgs/derivation.png";
+import growth from "../../public/svgs/growth.png";
 const marqueeFeatures = [
   "Automate your Trades",
   "Instant Transactions",
@@ -143,8 +148,8 @@ export default function Home() {
 
         <div className="max-w-[1440px] mx-auto  px-[60px]">
           <div className="pb-[96px] pt-[44px] grid grid-cols-3 gap-[25px]  items-center relative z-[40]">
-            <div className="col-span-1 h-[351px] grid-card relative">
-              <div className=" w-full grid-inner-card h-full absolute top-[0.5px] left-[1px]">
+            <div className="col-span-1 h-[397px] grid-card relative">
+              <div className=" w-full grid-inner-card h-full absolute top-[1px] left-[1px]">
                 <div className="p-8">
                   <h4 className="text-[#ffffff] text-[24px] font-[family-name:var(--font-inter)] font-medium">
                     A Platform for Every Skill Level
@@ -189,8 +194,8 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="col-span-2 h-[351px] grid-card relative">
-              <div className=" w-full grid-inner-card h-full absolute top-[0.5px] ">
+            <div className="col-span-2 h-full grid-card relative">
+              <div className=" w-full grid-inner-card h-full absolute top-[1px] ">
                 <div className="p-8">
                   <h4 className="text-[#ffffff] text-[24px] font-[family-name:var(--font-inter)] font-medium">
                     Elevate your Trading Startegy with AI
@@ -199,10 +204,22 @@ export default function Home() {
                     Insights provides instant access to public sentiment,
                     evolving market patterns, and trends.
                   </p>
+                  <div className="mt-[27px] flex relative">
+                    <Image
+                      className="w-[297px] object-contain"
+                      src={earn}
+                      alt="earn"
+                    />
+                    <Image
+                      className="w-[432px] object-contain -ml-[50px] mt-[100px]"
+                      src={grid}
+                      alt="grid"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-span-2 h-[351px] grid-card-2 relative">
+            <div className="col-span-2 h-[397px] grid-card-2 relative">
               <div className=" w-full grid-inner-card h-full absolute top-[1px] left-[1px]">
                 <div className="p-8">
                   <h4 className="text-[#ffffff] text-[24px] font-[family-name:var(--font-inter)] font-medium">
@@ -212,10 +229,22 @@ export default function Home() {
                     Chase AI is a decentralized, self-custodial platform that
                     simplifies derivatives trading with seamless AI automation.
                   </p>
+                  <div className="mt-[27px] flex relative">
+                    <Image
+                      className="w-[298px] h-[312px] object-contain"
+                      src={derivative}
+                      alt="derivative"
+                    />
+                    <Image
+                      className="w-[313px] h-[142px] object-contain -ml-[50px] mt-[80px]"
+                      src={growth}
+                      alt="growth"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-span-1 h-[351px] grid-card-3 relative">
+            <div className="col-span-1 h-[397px] grid-card-3 relative">
               <div className=" w-full grid-inner-card h-full absolute top-[1px] left-[1px] right-[1px]">
                 <div className="p-8">
                   <h4 className="text-[#ffffff] text-[24px] font-[family-name:var(--font-inter)] font-medium">
@@ -224,6 +253,9 @@ export default function Home() {
                   <p className="text-[#E6ECFFB2] text-[16px] font-[family-name:var(--font-inter)] font-normal mt-[18px]">
                     Gain Exposure to crypto with Perpetual Contracts.
                   </p>
+                  <div className="mt-[27px] flex relative justify-center">
+                    <Image src={oneclick} alt="oneclick" />
+                  </div>
                 </div>
               </div>
             </div>
